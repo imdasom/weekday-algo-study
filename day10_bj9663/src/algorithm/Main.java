075f82e51aeb6684a0187b732efd7ed380b9e8c8
imdasom <imdasom101@gmail.com>
2016-12-12 20:57:13 +0900

    2016.12.07 BFS, 심화문제, 백준9663 N-Queens, 풀이 실패! 백트래킹으로 풀어야 함.

---
+++ day10_bj9663/src/algorithm/Main.java
@@ -0,0 +1,14 @@
+package algorithm;
+
+/**
+ * Created by dasom on 2016-12-07.
+ *
+ * 이 문제는 백트래킹으로 풀어야 한다.
+ * 다음에 다시 시도!
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+}
